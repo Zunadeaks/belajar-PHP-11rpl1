e0cc4c3296417899615fa811bb19aa030c5db583
Zunadea <110004840+Zunadeaks@users.noreply.github.com>
2022-09-05 13:59:47 +0700

    Add files via upload

---
+++ if-for.php
@@ -0,0 +1,67 @@
+<?php
+        // Materi Percabangan (IF ELSE)
+        $nilai = 78;
+        echo "Nilai kamu adalah $nilai <br>";
+        echo "Maka status kamu = ";
+        if ($nilai > 78) {
+            echo "Lulus Beneran";
+        } else if ($nilai == 78){
+            echo "Lulus KKM";
+        } else {
+            echo "Tidak Lulus";
+        }echo "<hr>";
+
+        $nilai_web = 79;
+        $nilai_pbo = 82;
+        echo "Nilai WEB KAMU = $nilai_web <br>";
+        echo "Nilai PBO KAMU = $nilai_pbo <br>";
+        echo "Kelulusan kamu = ";
+        if ($nilai_web >=80 AND $nilai_pbo >= 80) {
+            echo "Lulus 2 Mapel Produktif";
+        } else if ($nilai_web >= 80 OR $nilai_pbo >=80) {
+            if ($nilai_web >= 80) {
+                echo "Lulus Mapel WEB";
+            }
+            if ($nilai_pbo >= 80) {
+                echo "Lulus Mapel PBO";
+            }
+        } else {
+            echo "Tidak Lulus Mapel Produktif";
+        }
+        echo "<hr>";
+        // Materi Perulangan (while, do while,for)
+        $i =1;
+        while ($i <= 5){
+            echo "Hello Word ! ke-$i <br>";
+            $i++; //Assigment +1 
+        }
+        echo "<hr>";
+        $j = 1;
+        do {
+            echo $j . "<br>";
+            $j++;
+        } while ($j <= 5);
+        echo "<hr>";
+
+        for ($k = 1; $k <=10; $k++){
+            echo $k . "<br>";
+        }
+        echo "<hr>";
+        for ($x = 9; $x >= 1; $x--){
+            for ($y = 1; $y <= $x; $y++){
+                echo $y;
+            }
+            echo "<br>";
+        }
+        echo "<hr>";
+        for ($z = 1; $z <= 10; $z++){
+            if ($z % 2==0) {
+                echo $z . "- Genap <br>";
+            } else {
+                echo $z . "-Ganjil <br>";
+            }
+        }
+
+
+
+
